ceaf31e38fa7c972cece41643b07cd6291056333
Aron Lindberg <aron.lindberg@case.edu>
2015-09-02 09:57:44 -0400

    automatic installing/loading of packages

--- analysis/0_load_packages.R
+++ analysis/0_load_packages.R
@@ -1,9 +1,5 @@
-library(gdata)
-library(poweRlaw)
-library(dunn.test)
-library(plyr)
-library("devtools")
-library(dplyr)
-library(tidyr)
-library(stringi) 
-library(lavaan)
+# This loads all the packages which are required for all other scripts
+required_packages <- c('gdata','poweRlaw','dunn.test', 'plyr', 'devtools', 'dplyr', 'tidyr', 'stringi', 'lavaan')
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,'Package'])]
+if(length(new_packages)) install.packages(new_packages)
+lapply(required_packages, library, character.only=T)
