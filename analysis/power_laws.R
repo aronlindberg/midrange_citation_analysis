b91325554fa4516f57ec4dfe8be5abe0b14ac81c
Aron Lindberg <aron.lindberg@case.edu>
2015-11-17 16:19:34 -0500

    renaming

--- analysis/power_laws.R
+++ analysis/power_laws.R
@@ -1,7 +1,6 @@
 # Comparing power-law distributions
 # http://cran.r-project.org/web/packages/poweRlaw/vignettes/compare_distributions.pdf
 
-
 instantiation_total_cites[instantiation_total_cites==0] <- 1
 m1 <- displ$new(instantiation_total_cites)
 m1 = displ$new(instantiation_total_cites)
@@ -25,13 +24,12 @@ comp12$p_two_sided
 comp13$p_two_sided
 comp23$p_two_sided
 
-
 kruskal.test(data$Total.Citations.by..ISI.Web.of.Science...SSCI. ~ data$Classification..instantiation..modifying..or.extending)
 
 dunn <- dunn.test(data$Total.Citations.by..ISI.Web.of.Science...SSCI., g=data$Classification..instantiation..modifying..or.extending)
 
 # Analysis for exploration/exploitation
-kruskal.test(data$Total.Citations.by..ISI.Web.of.Science...SSCI. ~ data$Classification..Exploitation.Exploration)
+kruskal.test(data$Total.Citations.by..ISI.Web.of.Science...SSCI. ~ data$X4_Classification..Exploitation.Exploration)
 
 dunn <- dunn.test(data$Total.Citations.by..ISI.Web.of.Science...SSCI., g=data$Classification..Exploitation.Exploration)
 
@@ -49,7 +47,30 @@ m2$setPars(estimate_pars(m2))
 m1$pars
 m2$pars
 
+
+# Extracting alphas adjusted for h1_index
+exploitation_total_cites_h1_2013_sum_adjusted <- subset(data$Total.Citations.by..ISI.Web.of.Science...SSCI., data$X4_Classification..Exploitation.Exploration == "Exploitation")/subset(data$h1_2013_sum, data$X4_Classification..Exploitation.Exploration == "Exploitation")
+
+exploration_total_cites_h1_2013_sum_adjusted <- subset(data$Total.Citations.by..ISI.Web.of.Science...SSCI., data$X4_Classification..Exploitation.Exploration == "Exploration")/subset(data$h1_2013_sum, data$X4_Classification..Exploitation.Exploration == "Exploration")
+  
+  
+exploitation_total_cites_h1_2013_sum_adjusted[exploitation_total_cites_h1_2013_sum_adjusted==0] <- 1
+m1 <- conpl$new(exploitation_total_cites_h1_2013_sum_adjusted)
+m1$setPars(estimate_pars(m1))
+
+exploration_total_cites_h1_2013_sum_adjusted[exploration_total_cites_h1_2013_sum_adjusted==0] <- 1
+m2 <- conpl$new(exploration_total_cites_h1_2013_sum_adjusted)
+m2$setPars(estimate_pars(m2))
+
+m1$pars
+m2$pars
+
+library(moments)
+skewness(exploitation_total_cites_h1_2013_sum_adjusted)
+skewness(exploration_total_cites_h1_2013_sum_adjusted)
+
 # Plotting
+library(ggvis)
 as.data.frame(exploitation_total_cites) %>% ggvis(~exploitation_total_cites) %>% layer_histograms() %>% scale_numeric("x", domain = c(0, 800), nice = FALSE, clamp = TRUE) %>% scale_numeric("y", domain = c(0, 25), nice = FALSE, clamp = TRUE)
 as.data.frame(exploration_total_cites) %>% ggvis(~exploration_total_cites) %>% layer_histograms() %>% scale_numeric("x", domain = c(0, 800), nice = FALSE, clamp = TRUE) %>% scale_numeric("x", domain = c(0, 800), nice = FALSE, clamp = TRUE)
 
@@ -60,4 +81,4 @@ axis(2,at=(0:800)*100)
 
 boxplot(exploration_total_cites, ylim=c(0, 800), axes=FALSE, frame.plot=FALSE)
 axis(1,at=0:2) 
-axis(2,at=(0:800)*100) 
+axis(2,at=(0:800)*100)
