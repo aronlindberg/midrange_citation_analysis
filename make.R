db2dcb41f4540da546cd4874893eb9d853e9176f
Aron Lindberg <aron.lindberg@case.edu>
2015-11-17 16:19:03 -0500

    move loading to make-file

--- make.R
+++ make.R
@@ -1,5 +1,13 @@
+setwd("/Users/aron/git/midrange_citation_analysis/")
+
 # This loads all the packages which are required for all other scripts
 required_packages <- c('gdata','poweRlaw','dunn.test', 'plyr', 'devtools', 'dplyr', 'tidyr', 'stringi', 'lavaan')
 new_packages <- required_packages[!(required_packages %in% installed.packages()[,'Package'])]
 if(length(new_packages)) install.packages(new_packages)
 lapply(required_packages, library, character.only=T)
+
+# Run scripts
+script_list <- list("load_data.R", "power_laws.R", "regressions.R", "growth_modeling.R")
+for (i in 1:length(script_list)){
+  try(source(script_list[[i]]))
+}
